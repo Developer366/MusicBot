a4f88a92583ef5463c38b0e226871a9e14c6b628
Kamil <peza361@gmail.com>
2023-04-08 13:19:57 -0400

    discordjs guide

--- server.js
+++ server.js
@@ -1,27 +1,49 @@
-//import discord.js classes
-//node server.js "to run the discord bot"
-const { Client, Events, GatewayIntentBits, SlashCommandBuilder} = require('discord.js');
-const Discord = require('discord.js');
-//const Client = new Discrod.Client();
+const fs = require('node:fs');
+const path = require('node:path');
+const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
+const { token } = require('./config.json');
 
-var invite = "https://discord.com/api/oauth2/authorize?client_id=1093926769368776786&permissions=40671259392832&scope=bot";
-var token = "MTA5MzkyNjc2OTM2ODc3Njc4Ng.G2eeWR.grg9znrNlaR7gRILwxz6MV5P47f65BJ4soDG9Y";
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-const client = new Client({intents: [GatewayIntentBits.Guilds] });
+client.commands = new Collection();
+const foldersPath = path.join(__dirname, 'commands');
+const commandFolders = fs.readdirSync(foldersPath);
 
-//guild means discrod server
-//when client is ready run code to login. You LOgin the MusicBot1, then start the commands "Always logged in unless the bot breaks"
-client.once(Events.ClientReady, c => {
-    console.log(`Ready! Logged in as ${c.user.tag}`)
-})
-client.login(token)
-
-data: new SlashCommandBuilder()
-        .setName('ping')
-        .setDescription("Replies with Pong!"),
-    async execute(interation) {
-        await interaction.reply("PONGGG!!!"),
-        await interaction.reply(`This command was run by ${interaction.user.username}, who joined on ${interaction.member.joinedAt}.`);
+for (const folder of commandFolders) {
+	const commandsPath = path.join(foldersPath, folder);
+	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+	for (const file of commandFiles) {
+		const filePath = path.join(commandsPath, file);
+		const command = require(filePath);
+		if ('data' in command && 'execute' in command) {
+			client.commands.set(command.data.name, command);
+		} else {
+			console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+		}
+	}
 }
 
+client.once(Events.ClientReady, () => {
+	console.log('Ready!');
+});
 
+client.on(Events.InteractionCreate, async interaction => {
+	if (!interaction.isChatInputCommand()) return;
+
+	const command = client.commands.get(interaction.commandName);
+
+	if (!command) return;
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		if (interaction.replied || interaction.deferred) {
+			await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
+		} else {
+			await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+		}
+	}
+});
+
+client.login(token);
